d1ac436f63cab1247b72a197fd27cb1bbef228ef
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-10-07 12:58:46 +0200

    Output from installers
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -42,11 +42,13 @@ end
 batch 'install_miniforge' do
   user username
   password user_password
+  sensitive false
   code "start /wait \"\" #{miniforge_installer} /InstallationType=JustMe /RegisterPython=0 /S /D=#{miniforge_install}"
 end
 
 batch 'run help' do
   user username
   password user_password
+  sensitive false
   code "#{miniforge_exe} /help"
 end
